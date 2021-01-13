b35844c28ed35894e327eaeed4b2a1ddba0ec6ea
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-01-13 15:05:41 +0100

    fix: nollup

--- fragments/nollup/blueprint.js
+++ fragments/nollup/blueprint.js
@@ -8,13 +8,14 @@ module.exports = {
                 `!production && isNollup && Hmr({ inMemory: true, public: assetsDir, }), // refresh only updated code`,
                 '!production && !isNollup && livereload(distDir), // refresh entire window when code is updated',
             ],
+            watch: {
+                clearScreen: 'false',
+                buildDelay: '100',
+            }
         },
         packagejson: require('./template/package.json')
     }),
     events: {
-        afterConfig: ({ configs }) => {
-            
-        },
         afterPatch: (({ transform }) => {
             transform('rollup.config.js', str => str.replace(/.*import svelte from.*/, ''))
         })
