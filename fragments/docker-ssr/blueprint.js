1d1f0196b7408c5b22f41a1c4f97847759540244
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-02-05 12:01:19 +0100

    add @comments scripts->build:docker to docker-ssr

--- fragments/docker-ssr/blueprint.js
+++ fragments/docker-ssr/blueprint.js
@@ -1,6 +1,11 @@
 module.exports = {
   configs: () => ({
     packagejson: {
+      "@comments scripts": {
+        "build:docker": [
+          "Builds a docker image. To disable this from the",
+          "build step, you can rename it to 'build-docker'"]
+      },
       scripts: {
         'build:docker': 'docker build -t routify-app:latest .',
         'run:docker': 'docker run --name routify -d -p 80:5005 routify-app:latest'
