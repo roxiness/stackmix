17e188e9f7821395f0e8b50f326de213bb189219
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-02-07 16:43:38 +0100

    add fragments page

---
+++ fragments/base/template/src/pages/fragments/index.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { url, isActive, layout } from "@roxi/routify";
+  $: getClass = (path) => ($isActive(path) ? "active" : "");
+</script>
+
+<ul>
+  {#each $layout.children as { path, title }}
+    <li>
+      <a href={$url(path)} class={getClass(path)}> {title}</a>
+    </li>
+  {/each}
+</ul>
