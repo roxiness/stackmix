5a911ece8a17519eac245ee8aecfef0bbc183bd9
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-02-08 14:47:25 +0100

    better comments for mobile overlay

--- fragments/mobile/template/src/components/navigation/Overlay.svelte
+++ fragments/mobile/template/src/components/navigation/Overlay.svelte
@@ -2,33 +2,44 @@
   import { afterPageLoad, route } from "@roxi/routify";
   let overlayElem, elHandle, width, height;
 
-  $afterPageLoad(updateOverlay);
-  $: (width || height) && updateOverlay();
+  // let's update the overlay position when there's a page change or the width changes
+  $afterPageLoad(updateOverlayPosition);
+  $: (width || height) && updateOverlayPosition();
 
-  function updateOverlay(node) {
+  /**
+   * updates position of overlay
+   * @param node the node being navigated to
+   */
+  function updateOverlayPosition(node) {
     const activeElem = elHandle.getElementsByClassName("is-active")[0];
-    const wasVisible = overlayElem.style.display !== "none";
-
-    overlayElem.style.display = "none";
 
     if (activeElem) {
+      const wasVisible = overlayElem.style.display !== "none";
+      // show the overlay
       overlayElem.style.display = null;
       const { x, y, width, height } = activeElem.getBoundingClientRect();
-      const movingOnY = parseInt(y) === parseInt(overlayElem.style.top);
-      console.log(movingOnY , node , wasVisible)
-      overlayElem.style.transition = movingOnY && node && wasVisible ? "all 0.3s" : "none";
+      const sameY = parseInt(y) === parseInt(overlayElem.style.top);
+      // animate only if the overlay has same y position as target
+      // & we're navigating to a node & the overlay was previously visible
+      overlayElem.style.transition = sameY && node && wasVisible ? "all 0.3s" : null;
       overlayElem.style.top = `${y}px`;
       overlayElem.style.left = `${x}px`;
       overlayElem.style.width = `${width}px`;
       overlayElem.style.height = `${height}px`;
+    } else {
+      // hide the overlay if there's no active link
+      overlayElem.style.display = "none";
     }
   }
 </script>
 
-<svelte:window bind:innerWidth={width} bind:innerHeight={height} />
-
+<!-- wrap this overlay around its target -->
 <div bind:this={elHandle}>
   <slot />
 </div>
 
+<!-- the overlay -->
 <div class="overlay" bind:this={overlayElem} />
+
+<!-- keep track of window resizing -->
+<svelte:window bind:innerWidth={width} bind:innerHeight={height} />
