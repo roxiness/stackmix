1a74263ca3483d59eb4614941de99db14a11dde8
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-02-08 18:22:00 +0100

    fix tailwind

--- fragments/tailwindcss/template/src/pages/guide/tailwind.svelte
+++ fragments/tailwindcss/template/src/pages/guide/tailwind.svelte
@@ -1,2 +1,7 @@
 <h1 class="text-4xl">Tailwindcss</h1>
-<p>Instructions to follow</p>
+
+<div class="flex justify-center items-center">
+    <h1 class="text-xl md:text-2xl lg:text-5xl text-pink-500">It's running with tailwind!</h1>
+</div>
+
+<p>Instructions to follow...</p>
--- fragments/tailwindcss/template/src/pages/index.svelte.fragment.js
+++
@@ -1,3 +0,0 @@
-exports.patch = ({placeholders, configs, stringify}) => { 
-    placeholders.html.push("<div class='flex justify-center items-center'><h1 class='text-xl md:text-2xl lg:text-5xl text-pink-500'>It's running with tailwind!</h1></div>")
-}
