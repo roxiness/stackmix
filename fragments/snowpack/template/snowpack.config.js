432e6d7159fa0d085f6176f67d3d315b74a227e8
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-02-11 22:01:45 +0100

    fix snowpack

--- fragments/snowpack/template/snowpack.config.js
+++ fragments/snowpack/template/snowpack.config.js
@@ -2,16 +2,19 @@
 module.exports = {
   mount: {
     public: { url: '/', static: true },
-    src: { url: '/' },
+    src: { url: '/build' },        
     '.routify': { url: '/.routify' }
   },
+  "routes": [
+    { "match": "routes", "src": ".*", "dest": "/__app.html" },
+  ],
   plugins: [
     '@snowpack/plugin-svelte',
     '@snowpack/plugin-dotenv'],
-  install: [
-    /* ... */
-  ],
-  installOptions: {
+  packageOptions: {
+    knownEntrypoints: [
+      /* ... */
+    ],
     /* ... */
   },
   devOptions: {
