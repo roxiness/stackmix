f9a76d441de0f49994c75dafa2d82490398d340e
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-03-03 20:50:56 +0100

    update snowpack

--- fragments/snowpack/blueprint.js
+++ fragments/snowpack/blueprint.js
@@ -1,6 +1,19 @@
+/** @type {import('canvasit')['Blueprint']} */
 module.exports = {
     type: 'bundler',
-    configs: ({ getConfigString, stringify }) => ({        
-        packagejson: require('./template/package.json')
+    configs: () => ({
+        packagejson: require('./package.json')
     }),
+    hooks: {
+        afterPatch: ctx => {
+            const parts = ctx.parseImports(ctx.stringify(ctx.configs.svelte))
+            ctx.writeTo('svelte.config.js',`
+                    ${parts.imports.join('\n')}
+                    ${parts.declarations.join('\n')}
+                    const production = process.env.NODE_ENV === 'production'
+                    module.exports = ${parts.body}
+                `
+            )
+        }
+    }
 }
