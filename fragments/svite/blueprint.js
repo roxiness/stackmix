9efe311717d505dc08191a3ee94d772511902ce1
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-03-03 22:06:01 +0100

    svite [wip]

--- fragments/svite/blueprint.js
+++ fragments/svite/blueprint.js
@@ -1,6 +1,10 @@
+// @ts-check
+
+/** @type {import('canvasit')['Blueprint']} */
 module.exports = {
+    imports: {'svelte': ['@svitejs/vite-plugin-svelte']},
     type: 'bundler',
-    configs: ({ getConfigString, stringify }) => ({
+    configs: ({ getConfigString, $require }) => ({
         packagejson: require('./package.json'),
         svelte: {
             emitCss: 'true',
@@ -11,8 +15,20 @@ module.exports = {
                 exclude: ["'@roxi/routify'"]
             },
             plugins: [
-                `svelte(${getConfigString('svelte')})`
+                $require('svelte')(getConfigString('svelte'))
             ]
         }
     }),
+    hooks: {
+        afterPatch: ctx => {
+            const sviteParts = ctx.parseImports(ctx.stringify(ctx.configs.vite))
+            ctx.writeTo('vite.config.js',`
+                    ${sviteParts.imports.join('\n')}
+                    ${sviteParts.declarations.join('\n')}
+                    const production = process.env.NODE_ENV === 'production'
+                    module.exports = ${sviteParts.body}
+                `
+            )
+        }
+    }
 }
