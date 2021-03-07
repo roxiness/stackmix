20444613e087923e2d8f5a37be8e1b0334f85cbe
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-03-07 14:01:33 +0100

    fix: added rollup guide

---
+++ fragments/rollup/template/src/pages/guide/rollup.svelte
@@ -0,0 +1,10 @@
+<h3>Rollup</h3>
+
+<p>Rollup is the default Svelte/Routify bundler.</p>
+<p>
+  It doesn't offer "no-bundling", but thanks to libraries like <code>nollup</code> it can offer the same lightning fast rebuilds.
+</p>
+<p>
+  Rollup runs automatically with <code>npm run dev</code> and <code>npm run build</code>.
+</p>
+<p>To run Rollup in isolation, run <code>npm run dev:rollup</code> or <code>npm run build:rollup</code></p>
