aefecdf44732f63ece336aca0c76eaeca2a2a757
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-03-16 08:26:53 +0100

    fix: navigation layout for single-page

--- fragments/single-page/template/src/pages/_layout.svelte
+++ fragments/single-page/template/src/pages/_layout.svelte
@@ -2,8 +2,6 @@
   import { components, beforeUrlChange, afterPageLoad, page } from "@roxi/routify";
   __SCRIPT__;
 
-  
-
   __IMPORTS__;
 
   __CONSTANTS__;
@@ -25,11 +23,15 @@ __HTML__
 </main>
 
 <nav>
-  __NAVIGATION__
-  {#each $components.find((n) => n.path === "/frontpage").children as node}
-    <a href="/#{node.title}">{node.title}</a>
-  {/each}
-  <a href="/guide">guide</a>
+  <div class="nav first">__NAVIGATIONFIRST__ __NAVIGATION__</div>
+  <div class="nav middle">
+    {#each $components.find((n) => n.path === "/frontpage").children as node}
+      <a href="/#{node.title}">{node.title}</a>
+    {/each}
+    <a href="/guide">guide</a>
+    __NAVIGATIONMIDDLE__
+  </div>
+  <div class="nav last">__NAVIGATIONLAST__</div>
 </nav>
 
 <style>
@@ -38,12 +40,17 @@ __HTML__
   }
   nav {
     position: fixed;
+    display: flex;
+    justify-content: space-between;
     top: 0;
     left: 0;
     right: 0;
     height: 50px;
-    text-align: center;
     padding: 8px;
+  }
+  nav div {
+    display: flex;
+    flex-basis: calc(100%/3);
   }
   nav a {
     margin: 0 8px;
