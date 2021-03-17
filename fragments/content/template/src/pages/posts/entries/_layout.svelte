b7888b9856d97a376eb41309374c14ef3961eb6a
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-03-17 18:40:31 +0100

    fix: redirect content's posts/entries to /posts

--- fragments/content/template/src/pages/posts/entries/_layout.svelte
+++ fragments/content/template/src/pages/posts/entries/_layout.svelte
@@ -1,2 +1,9 @@
+<!--
+     we're setting index to false because we don't want the entries folder to
+     be indexable (indexable components are iterable when traversing components).
+     Instead we're referencing it by the name "postEntries" from ../index.svelte. 
+    -->
+
+<!-- routify:options redirect="/posts" -->
 <!-- routify:options index=false -->
 <!-- routify:options name="postEntries" -->
