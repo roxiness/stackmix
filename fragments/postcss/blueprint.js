120fba837abf45be0f77356fd62f340b270bbe1d
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-03-17 11:26:25 +0100

    fix: postcss config can't be empty object

--- fragments/postcss/blueprint.js
+++ fragments/postcss/blueprint.js
@@ -6,6 +6,8 @@ module.exports = {
         autoPreprocess: {
             postcss: getConfig('postcss')
         },
-        postcss: {}
+        postcss: {
+            plugins: []
+        }
     })
 }
