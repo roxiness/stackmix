1c89bdca429d3465252770754d2ea8579f19a771
Ales Vaupotic <ales@vaupotic.eu>
2021-04-21 00:31:38 +0200

    replace 4 spaces with 2

--- fragments/auth/template/src/components/Auth/Link.svelte
+++ fragments/auth/template/src/components/Auth/Link.svelte
@@ -1,33 +1,33 @@
 <script>
-    import { user, authenticating } from "./store";
-    import { url } from "@roxi/routify";
+  import { user, authenticating } from "./store";
+  import { url } from "@roxi/routify";
 </script>
 
 <ul>
-    <li>
-        {#if $authenticating}
-            authenticating...
-        {:else if $user}
-            <a href={$url("/user")}>{$user.username}</a>
-        {:else}
-            <a href={$url("/login")}>Login</a>
-        {/if}
-    </li>
-    <li>
-        <a href={$url("/protected")}>Go to protected pages</a>
-    </li>
+  <li>
+    {#if $authenticating}
+      authenticating...
+    {:else if $user}
+      <a href={$url("/user")}>{$user.username}</a>
+    {:else}
+      <a href={$url("/login")}>Login</a>
+    {/if}
+  </li>
+  <li>
+    <a href={$url("/protected")}>Go to protected pages</a>
+  </li>
 </ul>
 
 <style>
-    ul.nested {
-        /* display: none; */
-        position: absolute;
-    }
-    ul.root:hover ul.nested {
-        display: block;
-    }
-    li {
-        margin: 1rem;
-        list-style: none;
-    }
+  ul.nested {
+    /* display: none; */
+    position: absolute;
+  }
+  ul.root:hover ul.nested {
+    display: block;
+  }
+  li {
+    margin: 1rem;
+    list-style: none;
+  }
 </style>
--- fragments/auth/template/src/pages/protected/_reset.svelte
+++ fragments/auth/template/src/pages/protected/_reset.svelte
@@ -1,66 +1,66 @@
 <script>
-    import Login from "../../components/Auth/Login.svelte";
-    import { authenticating, user, logout } from "../../components/Auth/store.js";
-    import { url, ready } from "@roxi/routify";
+  import Login from "../../components/Auth/Login.svelte";
+  import { authenticating, user, logout } from "../../components/Auth/store.js";
+  import { url, ready } from "@roxi/routify";
 
-    /**
-     * We don't want SSR for the content of protected pages. If we did,
-     * `$ready()` should be called if the user failed to authenticate.
-     * Otherwise Routify will wait indefinitely for `<slot />` to load.
-     * */
-    $: if (!$authenticating && !$user) $ready();
+  /**
+   * We don't want SSR for the content of protected pages. If we did,
+   * `$ready()` should be called if the user failed to authenticate.
+   * Otherwise Routify will wait indefinitely for `<slot />` to load.
+   * */
+  $: if (!$authenticating && !$user) $ready();
 </script>
 
 <div class="protected">
-    <div class="wrapper">
-        <nav>
-            <a href={$url("/")}>Go back</a>
-            {#if $user}<button on:click={logout}>logout</button>{/if}
-        </nav>
-        <h1>Protected module</h1>
-        <main class="container">
-            {#if $user}
-                <slot />
-            {:else if $authenticating}
-                <h2>authenticating...</h2>
-            {:else}
-                <Login />
-            {/if}
-        </main>
-    </div>
+  <div class="wrapper">
+    <nav>
+      <a href={$url("/")}>Go back</a>
+      {#if $user}<button on:click={logout}>logout</button>{/if}
+    </nav>
+    <h1>Protected module</h1>
+    <main class="container">
+      {#if $user}
+        <slot />
+      {:else if $authenticating}
+        <h2>authenticating...</h2>
+      {:else}
+        <Login />
+      {/if}
+    </main>
+  </div>
 </div>
 
 <style>
-    .protected {
-        position: absolute;
-        height: calc(100% - 16px);
-        width: calc(100% - 16px);
-        background: black;
-    }
+  .protected {
+    position: absolute;
+    height: calc(100% - 16px);
+    width: calc(100% - 16px);
+    background: black;
+  }
 
-    .wrapper {
-        padding: 2rem;
-    }
+  .wrapper {
+    padding: 2rem;
+  }
 
-    * :global(*) {
-        color: white;
-    }
-    * :global(code) {
-        color: yellow;
-        font-weight: bold;
-    }
+  * :global(*) {
+    color: white;
+  }
+  * :global(code) {
+    color: yellow;
+    font-weight: bold;
+  }
 
-    nav {
-        display: flex;
-        justify-content: space-between;
-        height: 2rem;
-        align-items: center;
-    }
+  nav {
+    display: flex;
+    justify-content: space-between;
+    height: 2rem;
+    align-items: center;
+  }
 
-    button {
-        padding: 8px;
-        color: black;
-        border-radius: 6px;
-        cursor: pointer;
-    }
+  button {
+    padding: 8px;
+    color: black;
+    border-radius: 6px;
+    cursor: pointer;
+  }
 </style>
