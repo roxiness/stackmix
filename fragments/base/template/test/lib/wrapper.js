ed714b06b0184a8a9e8139258c1d840b4c7fb886
Jakob Rosenberg <jakob@pycom.io>
2021-04-22 09:59:48 +0200

    add logging for ci tests

--- fragments/base/template/test/lib/wrapper.js
+++ fragments/base/template/test/lib/wrapper.js
@@ -7,44 +7,44 @@ const timeout = 15000 // 15s
 const npm = /^win/.test(process.platform) ? 'npm.cmd' : 'npm'
 const npx = /^win/.test(process.platform) ? 'npx.cmd' : 'npx'
 
-
 // assign `dev` or `build` to mode and `ava` to cmd
 const [mode, cmd, ...args] = process.argv.slice(2)
 
 const setups = {
-    async dev() {
-        console.log('[app] test dev')
-        const child = await spawn(npm, ['run', 'dev'])
-        const teardown = () => fkill(child.pid, { tree: true, force: true })
-        
-        const ready = await checkPort(port, timeout)
-        if (ready) {
-            await wait(500)
-            return teardown
-        } else {            
-            teardown()
-            throw Error(`dev on port ${port} timed out after ${timeout} ms`)
-        }
-    },
-    async build() {
-        console.log('[app] test build')
-        spawnSync(npm, ['run', 'build'],)
-        const child = await spawn(npm, ['run', 'serve'])        
-        const teardown = () => fkill(child.pid, { tree: true, force: true })
-        
-        if (await checkPort(port, timeout))
-            return teardown
-        else {
-            teardown()
-            throw Error(`build on port ${port} timed out after ${timeout} ms`)
-        }
+  async dev() {
+    console.log('[app] test dev')
+    const child = await spawn(npm, ['run', 'dev'])
+    console.log('[app] test completed.')
+    const teardown = () => fkill(child.pid, { tree: true, force: true })
+    
+    const ready = await checkPort(port, timeout)
+    if (ready) {
+      await wait(500)
+      return teardown
+    } else {
+      teardown()
+      throw Error(`dev on port ${port} timed out after ${timeout} ms`)
     }
+  },
+  async build() {
+    console.log('[app] test build')
+    spawnSync(npm, ['run', 'build'])
+    const child = await spawn(npm, ['run', 'serve'])
+    console.log('[app] test completed.')
+    const teardown = () => fkill(child.pid, { tree: true, force: true })
+    
+    if (await checkPort(port, timeout)) return teardown
+    else {
+      teardown()
+      throw Error(`build on port ${port} timed out after ${timeout} ms`)
+    }
+  }
 }
 
 async function wrapSpawn() {
-    const teardown = await setups[mode]()
-    spawnSync(npx, [cmd, ...args], { stdio: 'inherit' })
-    teardown()
+  const teardown = await setups[mode]()
+  spawnSync(npx, [cmd, ...args], { stdio: 'inherit' })
+  teardown()
 }
 
 wrapSpawn()
